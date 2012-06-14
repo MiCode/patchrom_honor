a6913df703a5f4083f4e70affed672af87f7754b
yumingyang <yumingyang@xiaomi.com>
2012-06-14 20:45:37 +0800

    rename framework2.jar.out to framework2
    Change-Id: I95b90b65258f73754d8c4e2a22664d7bdc514f2a

--- customize_framework.sh
+++ customize_framework.sh
@@ -9,16 +9,16 @@ APKTOOL=$PORT_ROOT/tools/apktool
 if [ $2 = "out/framework" ];then
 	echo "make framework2.jar"
 
-	mkdir -p "out/framework2.jar.out/smali"
-	touch "out/framework2.jar.out/apktool.yml"
-	echo "version: 1.4.3" >> "out/framework2.jar.out/apktool.yml"
-	echo "apkFileName: framework2.jar" >> "out/framework2.jar.out/apktool.yml"
+	mkdir -p "out/framework2/smali"
+	touch "out/framework2/apktool.yml"
+	echo "version: 1.4.3" >> "out/framework2/apktool.yml"
+	echo "apkFileName: framework2.jar" >> "out/framework2/apktool.yml"
 
-	mv "out/framework/smali/miui" "out/framework2.jar.out/smali"
-	mkdir -p "out/framework2.jar.out/smali/android"
-	mv "out/framework/smali/android/widget" "out/framework2.jar.out/smali/android"
+	mv "out/framework/smali/miui" "out/framework2/smali"
+	mkdir -p "out/framework2/smali/android"
+	mv "out/framework/smali/android/widget" "out/framework2/smali/android"
 
-	$APKTOOL b  "out/framework2.jar.out" "out/framework2.jar"
+	$APKTOOL b  "out/framework2" "out/framework2.jar"
 	mkdir -p "out/ZIP/system/framework"
 	cp "out/framework2.jar" "out/ZIP/system/framework/framework2.jar"
 fi
