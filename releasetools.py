380cee16499ba3bffb051f0af1baafd25303de0f
yumingyang <yumingyang@xiaomi.com>
2012-06-14 13:25:35 +0800

    modify script of patching boot.img
    Change-Id: I92ea8128cbfc53a7cb1bdcb9876376565017b82a

--- releasetools.py
+++ releasetools.py
@@ -17,8 +17,29 @@ def AddAssertions(info):
             edify.script[i] = edify.script[i].replace(");", ' || getprop("ro.product.device") == "u8860" || getprop("ro.build.product") == "u8860");')
             return
 
+def PatchBootImage(info):
+    print "Patch bootimg"
+    edify = info.script
+    for i in xrange(len(edify.script)):
+        if 'assert(apply_patch_check("EMMC:/dev/block/mmcblk0p1' in edify.script[i]:
+            #print "edify.scrip[", i, "]=", edify.script[i] 
+            s = edify.script[i].split(':')
+            e2 = s[3]
+            e1 = s[5].split('"')[0]
+            edify.script[i] = 'mount("vfat", "EMMC", "/dev/block/mmcblk0p1", "/cust");\n' + 'assert(apply_patch_check("/cust/image/boot.img", "' + e1 +'", "' + e2 + '"));'
+            info.script.Unmount("/cust")
+        elif 'apply_patch("EMMC:/dev/block/mmcblk0p1' in edify.script[i]:
+            #print "edify.scrip[", i, "]=", edify.script[i]
+            s = edify.script[i].split(',')
+            s[0] = 'apply_patch("/cust/image/boot.img"'
+            edify.script[i] = '';
+            for j in xrange(len(s) - 1):
+                edify.script[i] = edify.script[i] + s[j] + ','
+            edify.script[i] = edify.script[i] + s[len(s)-1]
+
 def FullOTA_InstallEnd(info):
     AddAssertions(info)
 
 def IncrementalOTA_InstallEnd(info):
     AddAssertions(info)
+    PatchBootImage(info)
